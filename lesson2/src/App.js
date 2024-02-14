d3426cbcd0421c9e85b11e71f71955883cb24fa8
UsmanalievI <uin.2201@gmail.com>
2024-02-14 18:34:22 +0600

    lesson2

--- lesson2/src/App.js
+++ lesson2/src/App.js
@@ -1,14 +1,14 @@
 import './App.css';
-import Button from './componentss/button/Button';
-import User from './componentss/user/User';
-import Example from './componentss/example/example';
-import Header from './componentss/header/Header';
-import Modal from './componentss/modal/Modal';
+// import Button from './componentss/button/Button';
+// import User from './componentss/user/User';
+// import Example from './componentss/example/example';
+// import Header from './componentss/header/Header';
+// import Modal from './componentss/modal/Modal';
 import { useState } from 'react';
 import CountPage from './page/countPage/CountPage';
 
 function App() {
-  const navbar=['home', 'about us', 'contacts']; 
+  // const navbar=['home', 'about us', 'contacts']; 
   const [show, setShow]=useState(false)
 
   const handleShow=()=>{
